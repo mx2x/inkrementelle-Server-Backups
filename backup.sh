53cd63e1964e855cea51c2a3b85cdb67642b5ff2
s256 <sepschneider@gmail.com>
2016-09-05 15:42:35 +0200

    Fix delete of increment file
    timestamp.snar files been deleted before copied to "lastmonth.d" folder. This is a big issue if you need to recover from lastmonth backup.

--- backup.sh
+++ backup.sh
@@ -7,29 +7,60 @@ LASTMONTHDIR=lastmonth
 TSNAME=timestamp.snar
 BACKUPNAME=Backup
 DIRS="/var/vmail /var/www"
-
+ERRORCODE="0";
+EMAILBODY="0"
+EMAIL_FROM="backup@beonline.business"
+EMAIL_TO="schneider@beonline.business"
+EMAIL_SUBJ_WARN="WARNING: Check Backup of mail01.beonline.business "
+EMAIL_SUBJ_SUCC="SUCCESS: Backup of mail01.beonline.business succeeded" 
 if [ $1 == "complete" ]; then
     #Komplettes Backup
     MYDATE=complete
-    #Alte Timestamps löschen
-    rm -f "$BACKUPDIR/$TSNAME"
+#    #Alte Timestamps löschen
+#    rm -fv "$BACKUPDIR"/*"$TSNAME"
     #Alte Backups löschen
-    rm -rf "$BACKUPDIR/$LASTMONTHDIR.$BACKUPNAME.d"
+    rm -rvf "$BACKUPDIR/$LASTMONTHDIR.$BACKUPNAME.d"
     #Neue alte Backups in Ordner verschieben
     mkdir "$BACKUPDIR/$LASTMONTHDIR.$BACKUPNAME.d"
-    mv -f "$BACKUPDIR/$BACKUPNAME.*".tgz "$BACKUPDIR/$LASTMONTHDIR.$BACKUPNAME.d"
+    mv -fv "$BACKUPDIR/$BACKUPNAME"* "$BACKUPDIR/$LASTMONTHDIR.$BACKUPNAME.d"
+    #Alte Timestamps löschen
+    rm -fv "$BACKUPDIR"/*"$TSNAME"
 else
     #Inkrementelles Backup
     MYDATE=$(date +%y%m%d)
 fi
-
+ 
 #Abzug erstellen
 for vhost in /var/www/*; do
-#echo "${file##*www/}"
-tar czf "$BACKUPDIR"/"$BACKUPNAME".Website.${vhost##*www/}.$MYDATE.tgz -g "$BACKUPDIR/$TSNAME" "${vhost}" 2> /dev/null
+#echo "${vhost##*www/}"
+tar czf "$BACKUPDIR"/"$BACKUPNAME".Website.${vhost##*www/}.$MYDATE.tgz -g "$BACKUPDIR/Web.${vhost##*www/}.$TSNAME" -C /var/ "${vhost##*var/}" 
+
+if [ $? -ne 0 ]; then
+ERRORCODE="1"
+fi
+
 done
 
-for vmail in /var/vmail/vhosts/*; do
-#echo "${file##*www/}"
-tar czf "$BACKUPDIR"/"$BACKUPNAME".Mail.${vmail##*vhosts/}.$MYDATE.tgz -g "$BACKUPDIR/$TSNAME" "${vmail}" 2> /dev/null
+for vmail in /var/vmail/*; do
+#echo "${vmail##*vmail/}" 2> /dev/null
+tar czf "$BACKUPDIR"/"$BACKUPNAME".Mail.${vmail##*vmail/}.$MYDATE.tgz -g "$BACKUPDIR/Mail.${vmail##*vmail/}.$TSNAME" -C /var/ "${vmail##*var/}" 
+
+if [ $? -ne 0 ]; then
+ERRORCODE="1"
+fi
+
 done
+
+
+if [ "$ERRORCODE" = "1" ]; then
+mail -aFrom:"${EMAIL_FROM}" -s "${EMAIL_SUBJ_WARN}" ${EMAIL_TO} <<EOM
+Ein Fehler ist aufgetreten ( ${MYDATE} ), bitte Backup kontrollieren.
+EOM
+
+else
+
+mail -aFrom:"${EMAIL_FROM}" -s "${EMAIL_SUBJ_SUCC}" ${EMAIL_TO} <<EOM
+Backup erfolgreich ( ${MYDATE} ).
+EOM
+ 
+fi
